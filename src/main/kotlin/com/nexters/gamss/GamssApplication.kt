c16a9c774e1b979371d2de056cd0c2b97b0e383d
MinJun Choi <mj043000@naver.com>
2026-07-12 01:43:35 +0900

    chore: 프로젝트 초기 설정
    Kotlin + Spring Boot 기반 서버 프로젝트를 Spring Initializr로 생성.
    - Spring Boot 4.1.0, Kotlin 2.3.21, Java 21 (toolchain)
    - Gradle Kotlin DSL, Spring Web 의존성

---
+++ src/main/kotlin/com/nexters/gamss/GamssApplication.kt
@@ -0,0 +1,11 @@
+package com.nexters.gamss
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamssApplication
+
+fun main(args: Array<String>) {
+	runApplication<GamssApplication>(*args)
+}
---
+++ src/test/kotlin/com/nexters/gamss/GamssApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nexters.gamss
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
