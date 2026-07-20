e1012fc7dc8554669385cef69a9e43b365664e64
kite707 <kite707707@gmail.com>
2026-07-21 00:55:57 +0900

    feat: 고아 PENDING 상태 정리 스케줄러 추가

---
+++ src/main/kotlin/com/nexters/gamss/global/config/SchedulingConfig.kt
@@ -0,0 +1,9 @@
+package com.nexters.gamss.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+/** @Scheduled 사용을 활성화한다. */
+@Configuration
+@EnableScheduling
+class SchedulingConfig
