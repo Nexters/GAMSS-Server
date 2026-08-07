7aedc548f099dfc5c0b365763e2efd862c324957
kite707 <kite707707@gmail.com>
2026-08-07 14:22:24 +0900

    refactor: TokenUsage 파일 분리

--- src/main/kotlin/com/nexters/gamss/tokenlimit/service/DailyTokenLimitService.kt
+++ src/main/kotlin/com/nexters/gamss/tokenlimit/service/DailyTokenLimitService.kt
@@ -56,10 +56,3 @@ class DailyTokenLimitService(
         private val ZONE: ZoneId = ZoneId.of("Asia/Seoul")
     }
 }
-
-/** [memberId]의 오늘 사용 토큰과 일일 상한. [dailyLimit]이 null이면 상한 미적용(무제한) 환경이다. */
-data class TokenUsage(
-    val usedTokens: Long,
-    val dailyLimit: Long?,
-    val exceeded: Boolean,
-)
